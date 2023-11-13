c68c4ffd3daa406fb4851aa7441fccfa16026831
Ricardo <ricardo.remond.pro@gmail.com>
2023-11-13 16:52:04 +0100

    exo 1 fini

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 const product = 'Chaussettes'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: true
+        }
+    },
+
+    methods: {
+        addToCart(id) {
+            this.cart.push(id)
+        },
+        remFromCart(id) {
+            this.cart.splice(id,1)
+        },
+
+
+    },
+
+    computed: {
+        image() { return this.variants[this.selectedVariant].image },
+        inStock() { return this.variants[this.selectedVariant].quantity }
+    }
+
+
+
+})
